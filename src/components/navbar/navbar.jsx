de4068d2781249e9757ae29ffb08eb36090808cf
Renaud <renaudfradin@gmail.com>
2022-12-26 13:51:07 +0100

    update: update style navbar v1

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,35 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import imgOpen from "../../assets/img/icon_nav/navigation.webp"
-import imgClose from "../../assets/img/icon_nav/navigation_close.webp"
+import { Link,NavLink } from "react-router-dom";
+import imgOpen from "../../assets/img/icon_nav/navigation.webp";
+import imgClose from "../../assets/img/icon_nav/navigation_close.webp";
+import imgHome from "../../assets/img/icon_nav/icon_main.webp";
+import imgBanner from "../../assets/img/icon_nav/icon_scout.webp";
+import imgCharacter from "../../assets/img/icon_nav/icon_character.webp";
+import imgWeapon from "../../assets/img/icon_nav/icon_weapon.webp";
+import imgEquipement from "../../assets/img/icon_nav/icon_equipment.webp";
+import imgImaginations from "../../assets/img/icon_nav/icon_scout.webp";
+import imgAbility from "../../assets/img/icon_nav/icon_scout.webp";
 import "./navbar.css"
 
 export default function Navbar(params) {
   return(
     <div className="layoutNavbar">
       <h3>SAOARS/UB</h3>
-      <div>
-        <img src="" alt="" />
+      <NavLink to='/' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+        <img src={imgHome} alt={imgHome} />
         <Link to='/'>Accueil</Link>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to='/banners' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+      <img src={imgBanner} alt={imgBanner} />
         <Link to='/banners'>Banniere</Link>
-      </div>
-      <div>
-      <Link to='/characters'>Personnage</Link>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to='/characters' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+        <img src={imgCharacter} alt={imgCharacter} />
+        <Link to='/characters'>Personnage</Link>
+      </NavLink>
+      <NavLink to='/weapons' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+        <img src={imgWeapon} alt={imgWeapon} />
         <Link to='/weapons'>Arme</Link>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to='/equipements' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+        <img src={imgEquipement} alt={imgEquipement} />
         <Link to='/equipements'>Equipement</Link>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to='/imaginations' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+        <img src={imgImaginations} alt={imgImaginations} />
         <Link to='/imaginations'>Imaginations</Link>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to='/abilitys' className={({ isActive })=> (isActive ? 'layoutIconNavActive' : 'layoutIconNav')}>
+        <img src={imgAbility} alt={imgAbility} />
         <Link to='/abilitys'>Capacité</Link>
-      </div>
+      </NavLink>
     </div>
   )
 }
