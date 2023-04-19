e74be3aaa2642ceb478819cdffb08b4cbb7d4d2a
Renaud <renaudfradin@gmail.com>
2023-04-19 18:31:36 +0200

    create : swiperHome and overview , and remose useEffect

--- src/hook/useBanner.jsx
+++ src/hook/useBanner.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import axios from "axios";
 
 export default function useBanner(id) {
@@ -18,7 +18,7 @@ export default function useBanner(id) {
     }
   };
 
-  useEffect(()=>{
+  useMemo(()=>{
     fetchBanner();
   }, [])
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "@assets/img/logoSAO.png";
 import imgDefault from "@assets/img/imgDefault/character_2502.png";
-import banner1 from "@assets/img/icon_ban/scout_132.png";
-import banner2 from "@assets/img/icon_ban/scout_133.png";
-import banner3 from "@assets/img/icon_ban/scout_134.png";
-import banner4 from "@assets/img/icon_ban/scout_135.png";
-import banner5 from "@assets/img/icon_ban/scout_136.png";
-import { Swiper, SwiperSlide } from "swiper/react";
-import bankLogo from "@assets/img/banklogo2.png";
+import SwiperHome from "@components/swiperHome/swiperHome";
+import Overview from "@components/overview/Overview";
 import { Helmet } from "react-helmet-async";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "./Home.css";
 
 export default function Home(){
-  const [bannerInfo, updateBannerInfo] = useState(100);
-  const [characterInfo, updateChracterInfo] = useState(100);
-  const [weaponInfo, updateWeaponInfo] = useState(100);
-  const [equipementInfo, updateEquipementInfo] = useState(100);
-  const [abilityInfo, updateAbilityInfo] = useState(100);
-
   return(
     <>
       <Helmet>
@@ -38,43 +24,8 @@ export default function Home(){
         <div className="divimgProm">
           <img src={imgDefault} alt={imgDefault} />
         </div>
-        <Swiper
-          slidesPerView={3}
-          className="swippeWrapper"
-          spaceBetween={10}
-        >
-          <SwiperSlide>
-            <img className="imgWrapper" src={banner1} alt={banner1} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="imgWrapper" src={banner2} alt={banner2} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="imgWrapper" src={banner3} alt={banner3} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="imgWrapper" src={banner4} alt={banner4} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="imgWrapper" src={banner5} alt={banner5} />
-          </SwiperSlide>
-        </Swiper>
-        <div className="containerOverview">
-          <p className="pOverview">DATABASE <span>:overview</span></p>
-          <div className="divOverview"> 
-            <div className="infoOverviewLeft">
-              <p>{bannerInfo} <span>banner</span></p>
-              <p>{characterInfo} <span>character</span></p>
-              <p>{weaponInfo} <span>weapon</span></p>
-            </div>
-            <img src={bankLogo} alt="" />
-            <div className="infoOverviewRight">
-              <p>{equipementInfo} <span>equipement</span></p>
-              <p>100 <span>imaginations</span></p>
-              <p>{abilityInfo} <span>abilitys</span></p>
-            </div>
-          </div>
-        </div>
+        <SwiperHome />
+        <Overview />
       </div>
       <div className="imgFooter">
         <div className="leftFooter" />
